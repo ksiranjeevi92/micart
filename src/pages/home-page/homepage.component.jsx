1c06f453e5df32575b30dd1c1b37b2a42fb8ed73
SIRANJEEVI <37017151+ksiranjeevi92@users.noreply.github.com>
2020-09-25 15:57:36 +0530

    first commit

---
+++ src/pages/home-page/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//components imports
+import Directory from './comonents/directory/directory.component';
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
